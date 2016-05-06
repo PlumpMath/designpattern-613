d5a5e1213315e1874f23cd3d4c89860828bf5034
suhao <simplesuhao@163.com>
2016-05-06 22:42:46 +0800

    2016-05-06

--- src/Filter/Main.java
+++ src/Filter/Main.java
@@ -3,6 +3,7 @@ package Filter;
 public class Main {
 	public static void main(String[] args) {
 		String msg = "大家好：）<script>,敏感，被就业，网络授课没感觉";
+
 		MsgProcessor mp = new MsgProcessor();
 		mp.setMsg(msg);
 		String result = mp.process();
